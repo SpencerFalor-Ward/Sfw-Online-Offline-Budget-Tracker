44671eb99dd4e30a52404bd4023e8612426b651d
SpencerFalor-Ward <srfalorward@ucdavis.edu>
2020-05-02 13:11:06 -0700

    work on service-worker file and htmlRoute

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,13 @@
-const FILES_TO_CACHE = ['/', '/index.html', 'index.js', 'favicon.ico']
+const FILES_TO_CACHE = [
+    '/',
+    '/index.html',
+    '/index.js',
+    '/favicon.ico',
+    './icons/icon-192x192.png',
+    './icons/icon-512x512.png',
+    '/style.css',
+    '/manifest.webmanifest',
+]
 
 const CACHE_NAME = 'static-cache-v2'
 const DATA_CACHE_NAME = 'data-cache-v1'
